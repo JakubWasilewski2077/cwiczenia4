25e5733056b5bc5f7f93730468d6794fee85e05e
MikSym <mikolajsym@gmail.com>
2023-03-15 15:40:40 +0100

    Initial commit

---
+++ cwiczenia4/main.py
@@ -0,0 +1,18 @@
+#zadanie1
+zbiorA = []
+for x in range(1,11):
+    zbiorA.append(1-x)
+print(zbiorA)
+
+zbiorB = []
+for x in range(8):
+    zbiorB.append(4**x)
+print(zbiorB)
+
+zbiorC = []
+for x in zbiorB:
+   if x % 2 == 0:
+       zbiorC.append(x)
+print(zbiorC)
+
+#zadanie2
